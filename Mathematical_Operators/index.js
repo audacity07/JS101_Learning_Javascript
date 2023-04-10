300f14d3aef95b93c0c51f7273cf5a2441ef5b16
ajeet p <ajeetpawar07@gmail.com>
2023-04-10 17:04:51 +0530

    mathematical operator problems

---
+++ Mathematical_Operators/index.js
@@ -0,0 +1,6 @@
+let a = 5;
+let b = 2;
+console.log(a + b); // 7
+console.log(a - b); // 3
+console.log(a * b); // 10
+console.log(a / b); // 2.5
---
+++ Mathematical_Operators/problem1.js
@@ -0,0 +1,5 @@
+let a= 15;
+
+let b= 5;
+
+console.log((a+b)+ (a+b)+(a+b)+ ((a/b)+(a-b)));
