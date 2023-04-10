6fb83d476212f864c26edee78e5a6fa7556e3f81
ajeet p <ajeetpawar07@gmail.com>
2023-04-10 17:13:07 +0530

    comparison operator problems

---
+++ Comparison_Operators/problem1.js
@@ -0,0 +1,10 @@
+let birthYear = 2000;
+let presentYear = 2023;
+let age = presentYear - birthYear;
+
+if (age >= 13 && age <= 19) {
+  console.log("Teenage");
+}
+else if (age >= 20 && age <= 29) {
+  console.log("Twenties");
+}
---
+++ Comparison_Operators/problem5.js
@@ -0,0 +1,27 @@
+let weekDay = "Fri";
+
+switch(weekDay) {
+  case "Mon":
+    console.log("Monday");
+    break;
+  case "Tue":
+    console.log("Tuesday");
+    break;
+  case "Wed":
+    console.log("Wednesday");
+    break;
+  case "Thur":
+    console.log("Thurday");
+    break;
+  case "Fri":
+    console.log("Friday");
+    break;
+  case "Sat":
+    console.log("Saturday");
+    break;
+  case "Sun":
+    console.log("Sunday");
+    break;
+  default:
+    console.log("Not a valid weekday");
+}
